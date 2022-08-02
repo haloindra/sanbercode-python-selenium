5f5a3b21e090ea9a034bbc2f585342347c4914a6
haloindra <96364035+haloindra@users.noreply.github.com>
2022-08-02 22:22:59 +0700

    Update Work Shift & Employment Status

--- Admin/Job/Job - Employment Status.py
+++ Admin/Job/Job - Employment Status.py
@@ -126,6 +126,46 @@ class AdminTest(unittest.TestCase):
 
         browser.find_element(By.NAME,"btnSave").click()
         time.sleep(3)
+    
+    def test_d_delete_employment_status(self):
+        #Login
+        browser = self.browser 
+        browser.get("https://opensource-demo.orangehrmlive.com/index.php/auth/login") 
+        time.sleep(3)
+        browser.find_element(By.NAME,"txtUsername").send_keys("Admin") 
+        time.sleep(1)
+        browser.find_element(By.NAME,"txtPassword").send_keys("admin123") 
+        time.sleep(1)
+        browser.find_element(By.NAME,"Submit").click() 
+        time.sleep(1)
+
+        browser.find_element(By.XPATH, "/html/body/div[1]/div[2]/ul/li[1]/a").click()
+        time.sleep(2)
+
+        #Hover to Job Title
+        job = browser.find_element(By.XPATH, "/html/body/div[1]/div[2]/ul/li[1]/ul/li[2]/a")
+        job_title = browser.find_element(By.XPATH, "/html/body/div[1]/div[2]/ul/li[1]/ul/li[2]/ul/li[1]/a")
+        pay_grade = browser.find_element(By.XPATH, "/html/body/div[1]/div[2]/ul/li[1]/ul/li[2]/ul/li[2]/a")
+        employ_stat = browser.find_element(By.XPATH, "/html/body/div[1]/div[2]/ul/li[1]/ul/li[2]/ul/li[3]/a")
+
+        actions = ActionChains(browser)
+        
+        #Click Pay Grade
+        actions.move_to_element(job).move_to_element(employ_stat).click().perform()
+        time.sleep(3)
+
+        #Validate URL
+        expected_current_url = "https://opensource-demo.orangehrmlive.com/index.php/admin/employmentStatus" 
+        self.assertEqual(expected_current_url, browser.current_url)
+
+        #Delete Employment Status
+        browser.find_element(By.XPATH, "/html/body/div[1]/div[3]/div[2]/div/div[2]/form/div[4]/table/tbody/tr[1]/td[1]/input").click()
+        time.sleep(1)
+        browser.find_element(By.NAME, "btnDelete").click()
+        time.sleep(1)
+        browser.find_element(By.ID, "dialogDeleteBtn").click()
+        time.sleep(1)
+
 
     def tearDown(self): 
         self.browser.close()
